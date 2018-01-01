ab2cd0fbb1dd07036583935a10972930de406583
Yingbo Cui <cuiyingbomail@163.com>
2018-01-01 20:51:33 +0800

    upload multiple single files at once

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, redirect, url_for, flash, render_template
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
-ALLOWED_EXTENSIONS = set(['zip'])
+ALLOWED_EXTENSIONS = set(['txt'])
 
 app = Flask(__name__)
 
@@ -18,24 +18,14 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            zip_ref = zipfile.ZipFile(os.path.join(UPLOAD_FOLDER, filename), 'r')
-            zip_ref.extractall(UPLOAD_FOLDER)
-            zip_ref.close()
-            return redirect(url_for('upload_file',
-                                    filename=filename))
+        file_count = request.files['count']
+        file_gene = request.files['gene']
+        file_sgrna = request.files['sgrna']
+        for file in [file_count, file_gene, file_sgrna]:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+        return redirect(url_for('upload_file',filename=filename))
     return render_template('index.html')
 
 
